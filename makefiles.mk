0ac476624864b25553f38f32e241501b8a6c961b
Jam Risser <jam@codejam.ninja>
2020-08-26 21:35:42 +0000

    Updated makefile

--- makefiles.mk
+++ makefiles.mk
@@ -1,44 +1,11 @@
 PLATFORM := $(shell node -e "process.stdout.write(process.platform)")
-SHX := node_modules/.bin/shx
 
 ifeq ($(PLATFORM), win32)
-	CAT := $(SHX) cat
-	CHMOD := $(SHX) chmod
-	CP := $(SHX) cp
-	FALSE := $(SHX) false
-	FIND := $(SHX) find
-	GREP := $(SHX) grep
-	LN := $(SHX) ln
-	LS := $(SHX) ls
 	MAKE = make
-	MKDIRP := $(SHX) mkdir
-	MV := $(SHX) mv
 	NULL := nul
-	PWD := $(SHX) pwd
-	RM := $(SHX) rm
-	SED := $(SHX) sed
 	SHELL = cmd.exe
-	TAIL := $(SHX) tail
-	TOUCH := $(SHX) touch
-	TRUE := $(SHX) true
 else
-	CAT := cat
-	CHMOD := chmod
-	CP := cp
-	FALSE := false
-	FIND := find
-	GREP := grep -E
-	LN := ln
-	LS := ls
-	MKDIRP := mkdir -p
-	MV := mv
 	NULL := /dev/null
-	PWD := pwd
-	RM := rm
-	SED := sed
-	TAIL := tail
-	TOUCH := touch
-	TRUE := true
   SHELL := $(shell bash --version >$(NULL) 2>&1 && echo bash|| echo sh)
 endif
 
